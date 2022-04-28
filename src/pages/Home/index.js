3ad7c402c4ae04d04d7434a4006b5ab2ce6c2621
JoaoRajiv <olivairarajiv@gmail.com>
2022-04-28 17:13:45 -0300

    Att do menu

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './styles.css';
 import poligono from '../../assets/polygon.svg';
-import imgMenu from '../../assets/img_inicio.png'
+import imgMenu from '../../assets/img_inicio.png';
+import retangulo from '../../assets/retangulo.png'
 
 export default function Home() {
     return (
             <div>
-                <img src={imgMenu} alt="fundo"></img>
+                <img src={retangulo} alt="fundo2" className="fundo2"></img>
+                <img src={imgMenu} alt="fundo" className="fundo"></img>
                 <section className="menu menu-content">
                 <nav className="logo">
                     <img src={poligono} alt="poligono"></img>
